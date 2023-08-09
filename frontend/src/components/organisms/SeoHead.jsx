a4cbcdeaf771d03ae00795ea035d1e474ceb8d66
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-08-09 14:30:48 -0300

    feat: static Seo

--- frontend/src/components/organisms/SeoHead.jsx
+++ frontend/src/components/organisms/SeoHead.jsx
@@ -9,9 +9,7 @@ const SeoHead = () => {
       <meta name="author" content="Tiago B. Braga" />
       <link rel="author" href="https://tiagodev.cia.tv" />
       <meta name="author" content="Tiago B. Braga" />
-      <meta name="keywords" content="Next.js,React,JavaScript,Portifólio" />
-      <meta name="referrer" content="unsafe-url" />
-      <meta name="color-scheme" content="light" />
+      <meta name="keywords" content="web,Portifólio,Next.js,React,JavaScript,Typescript" />      
       <meta name="creator" content="Tiago B. Braga" />
       <meta name="publisher" content="Tiago B. Braga" />
       <meta
@@ -34,28 +32,23 @@ const SeoHead = () => {
       <meta property="og:url" content="https://tiagodev.cia.tv/" />
       <meta property="og:site_name" content="Tiago Dev" />
       <meta property="og:locale" content="pt_BR" />
-      <meta property="og:image:url" content="/favicon.ico" />
       <meta property="og:image:width" content="800" />
       <meta property="og:image:height" content="600" />
-      <meta property="og:image:url" content="/favicon.ico" />
+      <meta property="og:image:url" content="/assets/whatsap.png" />
       <meta property="og:image:width" content="1800" />
       <meta property="og:image:height" content="1600" />
       <meta property="og:image:alt" content="Logo Tiago Dev" />
       <meta property="og:type" content="website" />
-      <meta name="robots" content="noindex, follow, nocache" />
+      <meta name="robots" content="follow, nocache" />
       <meta
         name="googlebot"
-        content="index, nofollow, noimageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
+        content="index, follow, imageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
       />
 
-      <link rel="shortcut icon" href="/images/02.png" />
-      <link rel="icon" href="/02.png" />
+      <link rel="shortcut icon" href="/assets/whatsap.pngg" />
+      <link rel="icon" href="/assets/whatsap.png" />
 
-      {/* <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
-      <link
-        rel="apple-touch-icon-precomposed"
-        href="/apple-touch-icon-precomposed.png"
-      /> */}
+     
     </Head>
   );
 };
