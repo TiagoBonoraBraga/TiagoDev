e1a0f178d1c007a9b3b0feee68fca0e5cad0399c
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-08-15 15:10:55 -0300

    feat: save

--- frontend/src/components/molecules/CardContacts.jsx
+++ frontend/src/components/molecules/CardContacts.jsx
@@ -1,12 +1,16 @@
-export default function CardContacts({ contactsList }) {
-   
+export default function CardContacts({ contacts }) {  
 
 
   return (
     <div className="flex flex-col  xs:flex-col   ">
       <div className="flex flex-row text-black-500 group-hover:text-white-500 text-sm font-thin mb-2 mr-5">
    
-            <div className="text-black-500 mr-2 mt-40">Nome:{} </div> 
+            <div className="text-black-500 mr-2 mt-40">
+            {contacts.map((contact) => (
+                <div className="text-black-500">{contact.name}</div>
+            ))}    
+                
+            </div> 
 
 
 
--- frontend/src/pages/contacts.jsx
+++ frontend/src/pages/contacts.jsx
@@ -1,36 +1,29 @@
-// import CardContacts from "@/components/molecules/CardContacts";
-// import { useState, useEffect } from "react";
-import {api} from  '@/utils/api'
+import CardContacts from "@/components/molecules/CardContacts";
+import { useState, useEffect } from "react";
+import { api } from "@/utils/api";
 
-export default function Contacts({data}) {
-//     const [contacts, setContacts] = useState([]);
+export default function Contacts() {
+  const [contacts, setContacts] = useState([]);
 
-//   async function ContactData() {
-//     const contacts = await api.getContacts();
-//     console.log('cont', contacts)
-//     setContacts(contacts);
-//   }
+  async function ContactsData() {
+    const contacts = await api.getContacts();
+    console.log("cont", contacts);
+    setContacts(contacts);
+  }
 
-//   useEffect(() => {
-//     ContactData()
-// }, []);
-  
-  return <div className="text-black-500">
+  useEffect(() => {
+    ContactsData();
+  }, [contacts]);
 
-   
-   
-<div className=" flex flex-wrap justify-center items-center max-w-screen-xl px-0 xl:px-16 mx-auto gap-16 xs:gap-3 xs:mb-16 mt-20">
-        {contactsList.map((contact) => (
-          <CardClients contact={contact} key={contact.id} />
-        ))}
-      </div>
-       
-   
-  </div>;
+  return (
+  <div className=" flex flex-wrap justify-center items-center max-w-screen-xl px-0 xl:px-16 mx-auto gap-16 xs:gap-3 xs:mb-16 mt-20">
+    <CardContacts contacts={contacts} />
+  </div>
+  )
 }
 
-export async function getServerSideProps() {
-    const data = await api.getContacts()
-    console.log(data)
-    return { props: { data } }
-  }
+// export async function getServerSideProps() {
+//     const data = await api.getContacts()
+//     console.log(data)
+//     return { props: { data } }
+//   }
