49a9fdcdb8816d26bf2d966fa571abc41260933e
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2023-09-06 16:29:32 -0300

    feat: CardProject style

--- frontend/src/components/molecules/CardProject.jsx
+++ frontend/src/components/molecules/CardProject.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 // import Fotoimage from "@/public/images/fotoimage.png";
 import {javascriptProjects} from '@/utils/mocks'
+import Link from "next/link";
 
 export default function CardProject() {
     console.log("aki",javascriptProjects )
@@ -8,27 +9,30 @@ export default function CardProject() {
   return (
     <>
         <h2 className="flex justify-center mt-10 text-lg font-extrabold">Projetos Javascript Vanilla</h2>
-      <section className="flex flex-row justify-center items-center ss:flex ss:flex-col gap-10">
+      <section className="flex flex-col justify-center items-center">
         {javascriptProjects.map((javascriptProject, index) => 
-        <div key={index} className="max-w-sm ring-2 mt-10 mb-10 ring-blue-200 bg-blue-100 bg-opacity-25 rounded-lg hadow dark:bg-black-500 dark:border-black-500">
-          <div className="flex justify-center items-center">
+        <div key={index} className="flex flex-row w-2/3  ss:flex ss:flex-col ss:max-w-sm ring-2 mt-3 mb-10 ring-blue-200 bg-blue-100 bg-opacity-25 rounded-lg ">
+          <div className="flex justify-center items-center p-3">
             <Image
                 src={javascriptProject.image}
-                width={300}
-                height={300}
+                width={800}
+                height={800}
                 alt="Project"
-                className="rounded mt-5 "
+                className="rounded"
             />
           </div>
+          <div>
+
           <h2 className="flex justify-center mb-2 mt-5 text-2xl font-bold tracking-tight text-black-500">
             {javascriptProject.title}
           </h2>
-          <p className="mt-2 mb-2 text-justify p-5">
+          <p className="mt-2 mb-2 text-justify p-5 ">
            {javascriptProject.description}
           </p>
-          <div className="flex flex-col justify-center items-center mb-5 gap-1">
-          <h3>Github - {javascriptProject.repository}</h3>
-          <h3>Deploy - {javascriptProject.deploy}</h3>
+          
+          <Link href="https://github.com/TiagoBonoraBraga/TiagoDev" className="flex justify-center items-center mb-3">Repositório GitHub</Link>
+          <Link href="https://www.foodbox.cia.tv" className="flex justify-center items-center mb-3">www.foodbox.cia.tv</Link>
+         
           </div>
         </div>
         )}
