44fb955b646a2d3ce68417f31477e2c41336c09e
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-09-27 12:06:11 -0300

    feat: add WordPressProjects in CardProjects

--- frontend/src/components/organisms/CardProject.jsx
+++ frontend/src/components/organisms/CardProject.jsx
@@ -2,14 +2,16 @@ import ReactProjects from "@/components/molecules/ReactProjects";
 import VanillaProjects from "../molecules/VanillaProjects";
 import NextProjects from "../molecules/NextProjects";
 import BackendProjects from "../molecules/BackendProjects";
+import WordPressProjects from "../molecules/WordPressProjects";
 
 export default function CardProject() {
   return (
     <>
       <section className="flex flex-col justify-center items-center">
-        <VanillaProjects />
         <ReactProjects />
         <NextProjects />
+        <VanillaProjects />
+        <WordPressProjects />
         <BackendProjects />
       </section>
     </>
