23483a3157afd86cc12ed908bfb293301f012ee4
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-10-02 16:07:13 -0300

    feat: change text about

--- frontend/src/components/molecules/CardAbout.jsx
+++ frontend/src/components/molecules/CardAbout.jsx
@@ -24,26 +24,24 @@ export default function CardAbout() {
                Tiago Bonora Braga - Desenvolvedor FullStack
              </div>
              <p className="text-gray-700 text-base text-justify">
-             Me chamo Tiago Bonora braga tenho 39 anos sou casado em moro em Curitiba,
-Venho de uma familia bem estruturada, o que me deu a oportunidade de focar nos estudos
-desde cedo, entrei na faculdade de Ed.Fisica ja com 17 anos e me formei com 21 anos, após
-esse periodo tambem realizei uma pós graduação na área e trabalhei por 8 anos com musculação
-e ed. especial como pss - processo seleção simplificado.
- Após esse periodo ja em 2012 comecei a sentir que o crescimento na area da ed. fisica estava limitado a aulas em academias e personal, e mesmo tendo especialização na area
-ainda continuava com a mesma remuneração de pessoas que nao tinham o mesmo grau de formação.
- Por esse motivo tive a oportunidade de mudar de area, para a area de vendas, visando uma 
-melhor colocação salarial. Abri uma empresa de vendas de pisos laminado em curitiba, pois minha familia vem desta area de vendas de artefatos para casa, chegando a ter 3 funcionarios.
- Essa experiencia foi muito valida , pois tive a oportunidade de gerenciar uma equipe
-e aprender muito com essa experiencia, e assim conseguindo atingir o lucro que desejava.
- Em 2016 quando começou a crise nesta area no brasil, sendo mutio dificil ter os lucros
-que estava tendo anteriormente. Então resolvi voltar a um sonho antigo que foi minha segunda opção quando entrei na faculdade de ed.fisica, que foi a area da t.i. Busquei cursos na area e vim me desenvolvendo juntamente com o trabalho com pisos laminados.
-ate que consegui um estagio na area no segundo semestre de 2021, atuando como frontend.
-ja no estagio fui encaminhado a fazer uma formação mais profunda na area
-Ate que fui indicado por uma amigo a fazer um curso de fullstack pela Blue Ed.tech aonde consegui aprimorar mais meus conhecimentos e ver que esta é minha paixao, a Tecnologia
-Atualmente trabalho como Freelancer na empresa Fpinfo como desenvolvedor Frontend e também na empresa BrGiga desenvolvendo sites em WordPress e WooCommerce.
-
-Me considero ágil e consistente em aprender uma nova linguagem e me adaptar a qualquer mudança, pronto para superar qualquer desafio.
-Se pudesse me definir em 3 palavras certamente seria: Otimista, Perseverante e Esforçado!
+              Me chamo Tiago Bonora Braga tenho 39 anos sou casado em moro em Curitiba,
+            venho de uma familia bem estruturada, o que me deu a oportunidade de focar nos estudos
+            desde cedo, entrei na faculdade de Ed.Fisica ja com 17 anos e me formei com 21 anos, após
+            esse periodo tambem realizei uma pós-graduação na área e trabalhei por 8 anos com musculação
+            e ed. especial como Pss - processo seleção simplificado Estado do Paraná.
+              Após esse periodo ja em 2012 comecei a sentir que o crescimento na área da ed. fisica estava limitado a aulas em academias e personal, 
+            e mesmo tendo especialização na área ainda continuava com a mesma remuneração de pessoas que nao tinham o mesmo grau de formação.
+              Por esse motivo tive a oportunidade de mudar novamente de área, agora para a área de vendas visando uma melhor colocação salarial. 
+              Abri uma empresa de vendas de pisos laminado em Curitiba, pois minha familia vem desta área de vendas de artefatos para casa, chegando a ter 3 funcionários,
+            essa experiência foi muito válida , pois tive a oportunidade de gerenciar uma equipe e aprender muito com essa experiência, assim conseguindo atingir o lucro que desejava.
+              Em 2016 quando começou a crise na área de serviços no brasil, sendo muito difícil obter os lucros que estava tendo anteriormente, resolvi voltar a um sonho antigo que foi minha segunda
+            opção quando entrei na faculdade de ed.fisica, que foi a área de T.I. 
+              Busquei cursos e fui me desenvolvendo juntamente com o trabalho de vendas de pisos laminados, até que consegui um estágio na área no segundo semestre de 2021 atuando como frontend.
+            já no estágio fui encaminhado a fazer uma formação mais profunda na área, sendo indicado por uma amigo a fazer um curso de fullstack pela Blue Ed.tech aonde consegui aprimorar mais meus conhecimentos 
+            e ver que esta é minha paixao, a Tecnologia.
+             Atualmente trabalho como Freelancer na empresa Fpinfo como desenvolvedor Frontend e também na empresa BrGiga desenvolvendo sites em WordPress e WooCommerce.
+              Me considero ágil e consistente em aprender uma nova linguagem e me adaptar a qualquer mudança, pronto para superar qualquer desafio.
+            Se pudesse me definir em 3 palavras certamente seria: Otimista, Perseverante e Esforçado!
              </p>
            </div>
            <div className="flex items-center">
