2e6970a166a1300cf8196dead3d9a8c24570cc1e
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-10-16 16:38:33 -0300

    feat: create and add CardContact in contact page

--- frontend/src/components/molecules/CardContact.jsx
+++ frontend/src/components/molecules/CardContact.jsx
@@ -1,17 +1,23 @@
-export default function CardContact({ contact }) {  
+export default function CardContact() {
 
   return (
-    <div className="flex flex-col  xs:flex-col   ">
-      <div className="flex flex-row text-blue-500  text-sm font-thin mb-2 mr-5">
-   
-            <div className="text-blue-500 mr-2 mt-40">
-              <p>{contact.email}</p>
-                
-            </div> 
-
-
-
+    <section className="flex flex-col justify-start items-start w-auto p-10 rounded ring-2 gap-5 bg-blue-100 ring-blue-200 text-white-500 mt-20 ss:gap-0 mb-60 ss:flex ss:flex-col ss:justify-center ss:items-center ">
+      <div className="flex flex-row px-5 py-1 rounded">
+        <div className="w-auto">
+          <h3 className="w-96">Nome:</h3>
+        </div>
+        <div>
+          <h3 className="w-96">Email:</h3>
+        </div>
+        <div>
+          <h3 className="w-48">Fone:</h3>
+        </div>
       </div>
-    </div>
+      <div className="flex flex-row w-auto px-5 py-1 rounded">
+        <h3 className="w-full">Mensagem:</h3>
+      </div>
+
+    </section>
+
   );
 }
