032eb3906638b74d26c3b6633bbbead9f3b1352e
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-10-17 16:48:13 -0300

    feat: create CustomLogo.jsx component

---
+++ frontend/src/components/atoms/CustomLogo.jsx
@@ -0,0 +1,8 @@
+export default function CustomLogo() {
+    return (
+    <>
+        <a href="/">
+            <h2 className="text-2xl font-bold text-white-500">Tiago Dev</h2>
+        </a>
+    </>)
+}
--- frontend/src/components/organisms/Header.jsx
+++ frontend/src/components/organisms/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <a href="javascript:void(0)">
+            <a href="/">
               <h2 className="text-2xl font-bold text-white-500">Tiago Dev</h2>
             </a>
             <div className="md:hidden">
