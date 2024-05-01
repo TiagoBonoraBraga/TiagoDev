7fc8a8bd041c004e199fb5496ad842cb630f76c2
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2024-05-01 10:20:28 -0300

    chore: change style customTitle

--- frontend/src/components/atoms/CustomTitle.jsx
+++ frontend/src/components/atoms/CustomTitle.jsx
@@ -1,3 +1,3 @@
 export default function CustomTitle({title}){
-    return <h1 className="text-start text-lg font-bold text-blue-300">{title}</h1>
+    return <h1 className="text-start text-lg font-bold text-blue-300 dark:text-black-500 mb-5">{title}</h1>
 }
